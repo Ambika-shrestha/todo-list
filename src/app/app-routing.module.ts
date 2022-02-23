f779a51dabb7382ee08c5c8a9122abe5f8d830b7
Ambika <ASA100@dsone.3ds.com>
2022-02-23 13:21:10 +0100

    (rename) added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { TodoHeaderComponent } from './components/todo-header/todo-header.component';
 
-const routes: Routes = [
-  { path: 'first-component', component: TodoHeaderComponent }
-];
+
 
 @NgModule({
   imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
+    CommonModule
   ],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoHeaderComponent } from './components/todo-header/todo-header.component';
+import { TodoHeader } from './components/todo-header/todo-header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations' 
-import { TodoListComponent } from './components/todo-list/todo-list';
-import { TodoBoxComponent } from './components/todo-box/todo-box';
+import { TodoList } from './components/todo-list/todo-list.component';
+import { Todo } from './components/todo/todo.component';
 import { TodoService } from './service/todoService';
-import { TodoFooterComponent } from './components/todo-footer/todo-footer';
+import { TodoFooter } from './components/todo-footer/todo-footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoHeaderComponent,
-    TodoListComponent,
-    TodoBoxComponent,
-    TodoFooterComponent
+    TodoHeader,
+    TodoList,
+    Todo,
+    TodoFooter
   ],
   imports: [
     BrowserModule,
